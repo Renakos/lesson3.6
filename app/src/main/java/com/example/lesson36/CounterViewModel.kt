c99547aac45db0af84ef72bb1389b5111c1a9afe
Renakos <145202397+Renakos@users.noreply.github.com>
2023-12-08 11:04:29 +0600

    Update CounterViewModel.kt

--- app/src/main/java/com/example/lesson36/CounterViewModel.kt
+++ app/src/main/java/com/example/lesson36/CounterViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-// Ваш ViewModel
 class CounterViewModel : ViewModel() {
     private val _counter = MutableLiveData<Int>()
     val counter: LiveData<Int> get() = _counter
